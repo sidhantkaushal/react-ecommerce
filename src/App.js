e2fa2de34cc53d4c34758aa3f2584cefd366bddc
sidhantkaushal <kaushalsidhant434@gmail.com>
2023-12-13 16:31:51 +0530

    Added Routing and Login Signup pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import Home from "./Pages/Home";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 }
